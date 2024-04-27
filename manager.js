658a6fe61868989873eff459d312f73b0649abd0
danehoov <DaneHoov22@gmail.com>
2024-04-27 12:18:09 -0600

    calculate bonuses

--- manager.js
+++ manager.js
@@ -19,6 +19,24 @@ class Manager extends Employee {
         this.employees.push(employee)
     }
 
+    calculateBonus(multiplier) {
+        let allEmployeeSalaries = this._totalSubSalary()
+        return (this.salary + allEmployeeSalaries) * multiplier
+    }
+
+    _totalSubSalary() {
+        let sum = 0
+
+        for (const employee of this.employees) {
+            if (employee instanceof Manager) {
+                sum += employee.salary + employee._totalSubSalary()
+            } else if (employee instanceof Employee) {
+                sum += employee.salary
+            }
+        }
+        return sum;
+    }
+
 }
 
 module.exports = Manager
